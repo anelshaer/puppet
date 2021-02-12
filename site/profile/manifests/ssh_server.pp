fd98cb8620c59b31e755b7622ced7ae77a2eaa46
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-12 12:53:51 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHckT7CPpUfDchIfqKp4GcXWwU5EBMoahrWU2Tz1jW1PeUL5LuhvOg+nEdZARzSfWI2hgkTh98R1+SN9QA7UVsuezmJwNJk3I6rbiBE+Qq/PKLIUuEDbqWsspKTWVVUoWdlvUSXtmBR9XXqTJDwUhfo7SCV/DfdM8qgM57jarobQd0qmdAESc4u7drcSg7Hi63dSdIEOSmVsKFIsDqsnC/qtv2fm6MYaMXQyANgvgnVNWhtjSF1NJNLQ5ryFNHr41aDcCM8AwU29NIs7cs29t1/TWQSUqsn2Gw3mWubf2C5FZPPjLtNArAbgYXGjtVya6xxRK75ZOlNWkair24o289 root@master.puppet.vm',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDHckT7CPpUfDchIfqKp4GcXWwU5EBMoahrWU2Tz1jW1PeUL5LuhvOg+nEdZARzSfWI2hgkTh98R1+SN9QA7UVsuezmJwNJk3I6rbiBE+Qq/PKLIUuEDbqWsspKTWVVUoWdlvUSXtmBR9XXqTJDwUhfo7SCV/DfdM8qgM57jarobQd0qmdAESc4u7drcSg7Hi63dSdIEOSmVsKFIsDqsnC/qtv2fm6MYaMXQyANgvgnVNWhtjSF1NJNLQ5ryFNHr41aDcCM8AwU29NIs7cs29t1/TWQSUqsn2Gw3mWubf2C5FZPPjLtNArAbgYXGjtVya6xxRK75ZOlNWkair24o289',
 	}
 }
