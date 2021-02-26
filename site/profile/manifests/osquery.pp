929c7f731563ddad9aaa3983ee1c35e908393887
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-26 21:55:30 +0100

    Update osquery.pp

--- site/profile/manifests/osquery.pp
+++ site/profile/manifests/osquery.pp
@@ -4,7 +4,7 @@ class profile::osquery (
   Enum['stopped', 'running'] $service  = lookup('profile::osquery::service', String, undef, 'running'),
   String  $cert                        = lookup('profile::osquery::cert', String, undef, ''),
   String  $enroll_secret               = lookup('profile::osquery::enroll_secret', String, undef, ''),
-  String  $tls_hostname                = lookup('profile::osquery::tls_hostname', String, undef, ''),
+  String  $tls_hostname                = lookup('profile::osquery::tls_hostname', String, undef, 'fleet.puppet.vm'),
   Boolean $enable_audit                = lookup('profile::osquery::enable_audit', Boolean, 'deep', false),
   Integer $events_max                  = lookup('profile::osquery::events_max', Integer, undef, 50000),
   Integer $events_expiry               = lookup('profile::osquery::events_expiry', Integer, undef, 3600),
