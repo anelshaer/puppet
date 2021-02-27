accfbc622eec734a7d794a4b6c141eb589ec3527
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-27 17:38:05 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -1,9 +1,10 @@
 class profile::docker_nodes {
   include docker
+  
   docker::run {'osquery01.puppet.vm':
-    
+    image  => 'centos',  
   }
   docker::run {'fleet.puppet.vm':
-    
+    image  => 'centos', 
   }
 }
