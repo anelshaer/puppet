2a1fa2a7be8934b28899dfb2c27884ed3e2ee933
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-27 18:32:37 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -10,6 +10,8 @@ class profile::docker_nodes {
   docker::run {'osquery01.puppet.vm':
     image            => 'centos',
     ensure           => present,
+    detach           => true,
+    command          => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
     extra_parameters => ['--interactive'],
     require => Docker::Image['centos'],
   }
