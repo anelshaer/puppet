7d315f8669f110d4db8dc8ca67eea141e1469c06
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-27 18:51:46 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -2,12 +2,24 @@ class profile::docker_nodes {
   include docker
   
    # fetch the docker image
+  docker::image { 'centos_fleetdm':
+    ensure    => 'present',
+    image_tag => '7',
+    docker_file => '/tmp/Dockerfile'
+    subscribe => File['/tmp/Dockerfile'],
+    require   => Class['docker'],
+  }
+
   docker::image { 'centos':
     ensure    => 'present',
     image_tag => '7',
     require   => Class['docker'],
   }
-  
+ file { '/tmp/Dockerfile':
+  ensure => file,
+  source => 'puppet:///modules/fleetdm/Dockerfile',
+}
+
   docker::run {'osquery01.puppet.vm':
     image            => 'centos:7',
     ensure           => present,
@@ -15,11 +27,12 @@ class profile::docker_nodes {
     extra_parameters => ['--interactive'],
     require => Docker::Image['centos'],
   }
+  
   docker::run {'fleet.puppet.vm':
     image            => 'centos:7',
     ensure           => present,
     ports            => ['443:443'],
     extra_parameters => ['--interactive'],
-    require => Docker::Image['centos'],
+    require => Docker::Image['centos_fleetdm'],
   }
 }
