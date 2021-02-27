889a8574871cedd46d2aecbd34cb7431d26f6cd8
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-27 17:55:58 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -4,6 +4,7 @@ class profile::docker_nodes {
   docker::run {'osquery01.puppet.vm':
     image    => 'centos',
     ensure   => present,
+    restart => 'always'
   }
   docker::run {'fleet.puppet.vm':
     image    => 'centos',
