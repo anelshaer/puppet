7296cf1867275f0d7e560c1ab02351b802d5c5e8
Ahmed Elshaer <4087461+anelshaer@users.noreply.github.com>
2021-02-27 18:05:11 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -1,13 +1,21 @@
 class profile::docker_nodes {
   include docker
   
+   # fetch the docker image
+  docker::image { 'centos':
+    ensure    => 'present',
+    require   => Class['docker'],
+  }
+  
   docker::run {'osquery01.puppet.vm':
     image    => 'centos',
     ensure   => present,
-    command  => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+    require => Docker::Image['centos'],
   }
   docker::run {'fleet.puppet.vm':
     image    => 'centos',
     ensure   => present,
+    ports   => ['443:443'],
+    require => Docker::Image['centos'],
   }
 }
